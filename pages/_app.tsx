004f712a4033d0682a12349d0144ec27bb28f6bd
FelineJTD <13520050@std.stei.itb.ac.id>
2023-02-14 11:54:56 +0700

    refactor: move typography css to text.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { Poppins } from "@next/font/google";
 const poppins = Poppins({subsets: ["latin"], weight: ["400", "600", "700"], variable: "--poppins"});
 import "@/styles/globals.css";
+import "@/styles/text.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
