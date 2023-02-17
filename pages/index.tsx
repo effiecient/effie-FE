0aeabaf0aded5e53f116cdb1e323bc78c8bb148d
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-02-17 08:31:10 +0700

    feat: create modal and update button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import Input from "@/components/input";
 import Button from "@/components/button";
+import { useState } from "react";
+import Modal from "@/components/modal";
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <>
       <Head>
@@ -22,6 +25,26 @@ export default function Home() {
         <a href="#">Link</a>
         <Button>Button</Button>
         <Input type="text" id="hello" name="hello" placeholder="Input" />
+        <Button
+          onClick={() => {
+            setIsModalOpen(!isModalOpen);
+          }}
+        ></Button>
+        {/* modal */}
+
+        <Modal
+          isOpen={isModalOpen}
+          onClose={() => {
+            setIsModalOpen(false);
+            console.log("close");
+          }}
+          onOutsideClick={() => {
+            setIsModalOpen(false);
+            console.log("outside");
+          }}
+        >
+          testing
+        </Modal>
       </main>
     </>
   );
