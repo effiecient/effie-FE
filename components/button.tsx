e53a279ef606805358a5fc9121d60311b48f1838
Adityapnn811 <apn725@gmail.com>
2023-02-23 21:19:06 +0700

    feat: navbar component

--- components/button.tsx
+++ components/button.tsx
@@ -1,18 +1,40 @@
 type ButtonProps = {
-  type?: "default" | "danger" | "success" | "warning" | "info";
+  type?: "default" | "danger" | "success" | "warning" | "info" | "custom";
   children?: React.ReactNode;
   onClick?: () => void;
   className?: string;
+  disabled?: boolean;
+  pill?: boolean;
 };
 
-export default function Button({ type = "default", children, onClick = () => {}, className }: ButtonProps) {
+export default function Button({
+  type = "default",
+  children,
+  onClick = () => {},
+  className,
+  disabled = false,
+  pill = false,
+}: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className={`${type === "danger" ? "bg-danger-400" : type === "success" ? "bg-success-400" : type === "warning" ? "bg-warning-400" : type === "info" ? "bg-info-400" : "bg-primary-500 text-white hover:bg-primary-700"}
-        rounded-md px-3 py-2 duration-300
+      className={`${
+        type === "danger"
+          ? "bg-danger-400"
+          : type === "success"
+          ? "bg-success-400"
+          : type === "warning"
+          ? "bg-warning-400"
+          : type === "info"
+          ? "bg-info-400"
+          : type === "custom"
+          ? ""
+          : "bg-primary-500 text-white hover:bg-primary-700"
+      }
+        ${pill ? "rounded-full" : "rounded-md"} px-3 py-2 duration-300
         ${className}
     `}
+      disabled={disabled}
     >
       {children}
     </button>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,25 +1,77 @@
 import { useState } from "react";
 import Login from "./auth/login";
 import Register from "./auth/register";
+import Button from "./button";
+import Image from "next/image";
+import loginIcon from "../public/icons/login.svg";
+import registerIcon from "../public/icons/register.svg";
+import directoriesIcon from "../public/icons/directories.svg";
+import userImg from "../public/images/user.png";
 
 export default function Navbar() {
-    const [isRegisterOpen, setIsRegisterOpen] = useState(false);
-    const [isLoginOpen, setIsLoginOpen] = useState(false);
+  const [isRegisterOpen, setIsRegisterOpen] = useState(false);
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    return (
-        <>
-            {/* Navbar */}
-            <div className="w-full bg-white">
-                <button onClick={() => setIsLoginOpen(!isLoginOpen)} disabled={isRegisterOpen}>Login</button>
-                <button onClick={() => setIsRegisterOpen(!isRegisterOpen)} disabled={isLoginOpen}>Register</button>
+  return (
+    <>
+      {/* Navbar */}
+      <div className="flex flex-row justify-between w-full bg-white h-[75px] items-center px-5">
+        <div>
+          <h1>Effie</h1>
+        </div>
+        <div className="space-x-1.5">
+          {isLoggedIn ? (
+            <div className="flex flex-row items-center gap-1.5">
+              <Button type="default" pill={true}>
+                <div className="flex flex-row gap-1.5 items-center">
+                  <Image src={directoriesIcon} alt="register icon" />
+                  My Directories
+                </div>
+              </Button>
+              <Image src={userImg} alt="user image" width={40} height={40} />
             </div>
+          ) : (
+            <>
+              <Button
+                onClick={() => setIsLoginOpen(!isLoginOpen)}
+                type="custom"
+                disabled={isLoginOpen}
+                className={
+                  "disabled:opacity-50 text-primary-500 bg-white border-primary-500 border-2"
+                }
+                pill={true}
+              >
+                <div className="flex flex-row gap-1.5 items-center">
+                  <Image src={loginIcon} alt="login icon" />
+                  Login
+                </div>
+              </Button>
+              <Button
+                onClick={() => setIsRegisterOpen(!isRegisterOpen)}
+                type="default"
+                disabled={isRegisterOpen}
+                className={"disabled:opacity-50"}
+                pill={true}
+              >
+                <div className="flex flex-row gap-1.5 items-center">
+                  <Image src={registerIcon} alt="register icon" />
+                  Register
+                </div>
+              </Button>
+            </>
+          )}
+        </div>
+      </div>
 
-            {/* Register */}
-            <Register isOpen={isRegisterOpen} onClose={() => setIsRegisterOpen(false)} />
+      {/* Register */}
+      <Register
+        isOpen={isRegisterOpen}
+        onClose={() => setIsRegisterOpen(false)}
+      />
 
-            {/* Login */}
-            <Login isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)} />
-        </>
-    );
+      {/* Login */}
+      <Login isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)} />
+    </>
+  );
 }
-  
