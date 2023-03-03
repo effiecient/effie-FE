a1a20c134c8c721a07fa2927070f0545c192a9f4
FelineJTD <13520050@std.stei.itb.ac.id>
2023-03-03 08:13:51 +0700

    fix: add on outside click

--- components/link-card.tsx
+++ components/link-card.tsx
@@ -27,10 +27,10 @@ export default function LinkCard({ content, title, url, effieUrl, onClick, class
             onClick={onClick}
             className={`
                 ${className} 
-                ${content.slice(0,3) === "new" ? "items-center justify-center gap-2 hover:bg-primary-50 hover:border-primary-50 cursor-pointer duration-200" : 
-                content.slice(0,7) === "display" ? "border-neutral-200" : 
-                "flex-col gap-1 hover:border-neutral-200 cursor-pointer"} 
-                group relative flex pt-3 pb-2 px-5 bg-white rounded-xl border border-white focus:border-primary-500 w-[32vw] md:w-[44vw] lg:w-[20vw] max-w-[16rem] min-w-[8rem] min-h-[4rem]`
+                ${content.slice(0,3) === "new" ? "items-center justify-center gap-2 border-white hover:bg-primary-50 hover:border-primary-50 cursor-pointer duration-200" : 
+                content.slice(0,7) === "display" ? "flex-col gap-1 border-neutral-200" : 
+                "flex-col gap-1 hover:border-neutral-200 cursor-pointer border-white"} 
+                group relative flex pt-3 pb-2 px-5 bg-white rounded-xl border focus:border-primary-500 w-[32vw] md:w-[44vw] lg:w-[20vw] max-w-[16rem] min-w-[8rem] min-h-[4rem]`
             }
         >
             {content === "new folder" ? (
@@ -44,26 +44,33 @@ export default function LinkCard({ content, title, url, effieUrl, onClick, class
                     <h6 className="text-primary-500">New link</h6>
                 </>
             ) : (
-                <>
-                    <h6 className="text-neutral-800">{title}</h6>
-                    <a 
-                        href={url} 
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="hover:text-primary-500 text-neutral-600 text-xs un hover:decoration-primary-500 inline-block max-w-full overflow-hidden"
-                    >
-                        {url}
-                    </a>
-                    {(content === "link" || content === "folder") && (
-                        <button className="group-hover:opacity-100 opacity-0 translate-x-1 group-hover:translate-x-0 absolute right-0 bottom-0 flex items-end h-full p-2 z-10 bg-white duration-100 rounded-r-xl" onClick={copyEffieUrl}>
-                            <CopyIcon className="duration-100 h-7 w-7" />
-                            {/* Copy success notif */}
-                            <div ref={copySuccessRef} className="opacity-0 -translate-y-1 absolute top-14 bg-neutral-900/50 text-white rounded-md py-1 px-2 shadow-lg text-left duration-300 max-w-[12rem]">
-                                <p className="text-xs">Link copied!<br /><span className="text-[0.6rem] underline text-neutral-100">{effieUrl}</span></p>
-                            </div>
-                        </button>
+                <div className="flex">
+                    {content === "link" || content === "display link" ? (
+                        <Image src="/icons/link.svg" alt="link" width={28} height={28} className="mr-2" />
+                    ) : (
+                        <div className="absolute left-0 top-0 h-full w-2" style={{ backgroundColor: "#FFF" }} />
                     )}
-                </>
+                    <div>
+                        <h6 className="text-neutral-800">{title}</h6>
+                        <a 
+                            href={url} 
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:text-primary-500 text-neutral-600 text-xs un hover:decoration-primary-500 inline-block max-w-full overflow-hidden"
+                        >
+                            {url}
+                        </a>
+                        {(content === "link" || content === "folder") && (
+                            <button className="group-hover:opacity-100 opacity-0 translate-x-1 group-hover:translate-x-0 absolute right-0 bottom-0 flex items-end h-full p-2 z-10 bg-white duration-100 rounded-r-xl" onClick={copyEffieUrl}>
+                                <CopyIcon className="duration-100 h-7 w-7" />
+                                {/* Copy success notif */}
+                                <div ref={copySuccessRef} className="opacity-0 -translate-y-1 absolute top-14 bg-neutral-900/50 text-white rounded-md py-1 px-2 shadow-lg text-left duration-300 max-w-[12rem]">
+                                    <p className="text-xs">Link copied!<br /><span className="text-[0.6rem] underline text-neutral-100">{effieUrl}</span></p>
+                                </div>
+                            </button>
+                        )}
+                    </div>
+                </div>
             )}
         </div>
     );
