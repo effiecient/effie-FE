99adcd328600156070dc2a3825af9cf1540e7d29
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-03-03 08:10:13 +0700

    wip: trying another way

--- components/side-modal.tsx
+++ components/side-modal.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 type SideModalProps = {
     isOpen: boolean;
@@ -13,27 +13,50 @@ export default function SideModal({
     children,
     className,
 }: SideModalProps) {
+    const [isHidden, setIsHidden] = useState(false);
+    useEffect(() => {
+        // with timer
+        if (!isOpen) {
+            setTimeout(() => {
+                setIsHidden(true);
+            }, 500);
+        } else {
+            setIsHidden(false);
+        }
+    }, [isOpen]);
     return (
         <div
             className={`${
-                isOpen ? "z-[100]" : "-z-10"
-            } fixed w-full duration-500`}
+                isOpen ? "top-0 right-0 w-full absolute h-full z-40" : "-z-10"
+            }`}
         >
             {/* Clickable Background */}
-            <div
+            {/* <div
                 className={`${
-                    isOpen ? "" : "opacity-0"
-                } fixed w-full h-full bg-black bg-opacity-50 backdrop-blur duration-500`}
+                    isOpen
+                        ? "w-full h-full absolute bg-black bg-opacity-50 backdrop-blur duration-500"
+                        : "opacity-0"
+                } `}
                 onClick={onClose}
-            />
-
-            {/* Modal */}
+            /> */}
             <div
-                className={`${className} ${
-                    isOpen ? "" : "motion-safe:translate-x-[100%] opacity-50"
-                } fixed bg-white h-full w-1/3 right-0 motion-safe:duration-500 ease-in-out p-12`}
+                // className={`${
+                //     isOpen ? "z-[100]" : "-z-10"
+                // } fixed w-full duration-500`}
+                className={` ease-in-out duration-300 w-1/3 fixed ${
+                    isOpen ? "translate-x-0" : "translate-x-[100%]"
+                }`}
             >
-                {children}
+                {/* Side Modal */}
+                <div
+                // className={`${className} ${
+                //     isOpen
+                //         ? ""
+                //         : "motion-safe:translate-x-[100%] opacity-50"
+                // } fixed bg-white h-full  right-0 motion-safe:duration-500 ease-in-out p-12`}
+                >
+                    {children}
+                </div>
             </div>
         </div>
     );
