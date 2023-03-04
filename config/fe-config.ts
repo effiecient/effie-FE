45c8379744e7c50c431fe78d1ff51a3ff69fb2cc
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-03-04 22:06:54 +0700

    chore: changing config to effie

--- config/fe-config.ts
+++ config/fe-config.ts
@@ -1,3 +1,7 @@
-export const FE_BASE_URL = "localhost.com:3000";
-export const FULL_FE_BASE_URL = `http://${FE_BASE_URL}`;
-export const FE_DOMAIN = "localhost.com";
+// export const FE_DOMAIN = "localhost.com";
+// export const FE_BASE_URL = `${FE_DOMAIN}:3000`;
+// export const FULL_FE_BASE_URL = `http://${FE_BASE_URL}`;
+
+export const FE_DOMAIN = "effie.boo";
+export const FE_BASE_URL = `${FE_DOMAIN}`;
+export const FULL_FE_BASE_URL = `https://${FE_BASE_URL}`;
