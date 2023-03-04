03024dea7fd053ec6e6e4aaf4c0cf49ebbf6500d
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-03-04 17:54:40 +0700

    fix: adding timer to z index in side modal, to prevent svg passing the side bar on close

--- components/side-modal.tsx
+++ components/side-modal.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 type SideModalProps = {
     isOpen: boolean;
     onClose: () => void;
@@ -11,24 +13,35 @@ export default function SideModal({
     children,
     className,
 }: SideModalProps) {
+    const [isHidden, setIsHidden] = useState(true);
+    useEffect(() => {
+        // give timer
+        if (isOpen) {
+            setIsHidden(false);
+        } else {
+            setTimeout(() => {
+                setIsHidden(true);
+            }, 300);
+        }
+    }, [isOpen]);
     return (
         <div
             className={`fixed left-0 w-full h-full top-[75px] ${
-                isOpen ? "z-10" : ""
+                isOpen ? "z-10" : isHidden ? "-z-10" : "z-10"
             }`}
         >
             {/* Clickable Background */}
             <div
                 className={`absolute ${
                     isOpen
-                        ? "w-full h-full bg-black bg-opacity-50 backdrop-blur duration-500"
+                        ? "w-full h-full bg-black bg-opacity-50 backdrop-blur duration-300"
                         : "opacity-0"
                 } `}
                 onClick={onClose}
             />
             {/* Side Modal */}
             <div
-                className={`${className} fixed right-0 ease-in-out duration-300 w-1/4 h-full bg-white p-6 ${
+                className={`${className} fixed right-0 ease-in-out duration-300 w-1/2 h-full bg-white p-6 ${
                     isOpen ? "translate-x-0" : "translate-x-[100%]"
                 }`}
             >
