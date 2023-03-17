2f6d4b8e8a8f8b57a2f2b54dc90b8060e9e1952a
Adityapnn811 <apn725@gmail.com>
2023-03-17 23:02:26 +0700

    fix: env tertiary condition

--- config/be-config.ts
+++ config/be-config.ts
@@ -1,6 +1,6 @@
 // export const BE_BASE_URL = "http://localhost:8080/api";
 export const BE_BASE_URL =
-    process.env.VERCEL_ENV === "production"
+    process.env.VERCEL_ENV == "production"
         ? "https://api.effie.boo/api"
         : "https://dev.api.effie.boo/api";
 
