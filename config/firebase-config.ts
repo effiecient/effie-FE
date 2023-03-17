afba69070fb4ddba7cd3b69740c2a0edb68829b4
Adityapnn811 <apn725@gmail.com>
2023-03-17 22:21:40 +0700

    Fix: changed VERCEL_PUBLIC_NODE_ENV to VERCEL_ENV

--- config/firebase-config.ts
+++ config/firebase-config.ts
@@ -1,30 +1,30 @@
 export const FIREBASE_CONFIG = {
+    // swap all of these object and change the condition to === "production"
+    // to use the production config
     apiKey:
-        process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
-            ? "AIzaSyDJxdWsoTvciQBfw2OIa0jvuR9l_xEAIls"
-            : "AIzaSyBIlSbwlKUovrLptRCcByKoesYU8rZXMlw",
+        process.env.VERCEL_ENV === "production"
+            ? "AIzaSyBIlSbwlKUovrLptRCcByKoesYU8rZXMlw"
+            : "AIzaSyDJxdWsoTvciQBfw2OIa0jvuR9l_xEAIls",
     authDomain:
-        process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
-            ? "effie-be-dev.firebaseapp.com"
-            : "effie-be.firebaseapp.com",
+        process.env.VERCEL_ENV === "production"
+            ? "effie-be.firebaseapp.com"
+            : "effie-be-dev.firebaseapp.com",
     projectId:
-        process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
-            ? "effie-be-dev"
-            : "effie-be",
+        process.env.VERCEL_ENV === "production" ? "effie-be" : "effie-be-dev",
     storageBucket:
-        process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
-            ? "effie-be-dev.appspot.com"
-            : "effie-be.appspot.com",
+        process.env.VERCEL_ENV === "production"
+            ? "effie-be.appspot.com"
+            : "effie-be-dev.appspot.com",
     messagingSenderId:
-        process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
-            ? "1047258247146"
-            : "1094450506359",
+        process.env.VERCEL_ENV === "production"
+            ? "1094450506359"
+            : "1047258247146",
     appId:
-        process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
-            ? "1:1047258247146:web:639f0e3011277a57cb8c93"
-            : "1:1094450506359:web:6f7b97eefc899603dd3472",
+        process.env.VERCEL_ENV === "production"
+            ? "1:1094450506359:web:6f7b97eefc899603dd3472"
+            : "1:1047258247146:web:639f0e3011277a57cb8c93",
     measurementId:
-        process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
-            ? "G-ERZQTH8T4V"
-            : "G-9670T0JENL",
+        process.env.VERCEL_ENV === "production"
+            ? "G-9670T0JENL"
+            : "G-ERZQTH8T4V",
 };
