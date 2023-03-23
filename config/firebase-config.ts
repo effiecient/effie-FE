97da027be9d6c510eb99a5c7e873f8e4c91a637f
Christopher Jeffrey <christopherjeffrey@gmail.com>
2023-03-23 17:17:00 +0700

    renaming node_env to vercel_env

--- config/firebase-config.ts
+++ config/firebase-config.ts
@@ -1,28 +1,28 @@
 export const FIREBASE_CONFIG = {
     apiKey:
-        process.env.NODE_ENV === "development"
+        process.env.VERCEL_ENV === "development"
             ? "AIzaSyDJxdWsoTvciQBfw2OIa0jvuR9l_xEAIls"
             : "AIzaSyBIlSbwlKUovrLptRCcByKoesYU8rZXMlw",
     authDomain:
-        process.env.NODE_ENV === "development"
+        process.env.VERCEL_ENV === "development"
             ? "effie-be-dev.firebaseapp.com"
             : "effie-be.firebaseapp.com",
     projectId:
-        process.env.NODE_ENV === "development" ? "effie-be-dev" : "effie-be",
+        process.env.VERCEL_ENV === "development" ? "effie-be-dev" : "effie-be",
     storageBucket:
-        process.env.NODE_ENV === "development"
+        process.env.VERCEL_ENV === "development"
             ? "effie-be-dev.appspot.com"
             : "effie-be.appspot.com",
     messagingSenderId:
-        process.env.NODE_ENV === "development"
+        process.env.VERCEL_ENV === "development"
             ? "1047258247146"
             : "1094450506359",
     appId:
-        process.env.NODE_ENV === "development"
+        process.env.VERCEL_ENV === "development"
             ? "1:1047258247146:web:639f0e3011277a57cb8c93"
             : "1:1094450506359:web:6f7b97eefc899603dd3472",
     measurementId:
-        process.env.NODE_ENV === "development"
+        process.env.VERCEL_ENV === "development"
             ? "G-ERZQTH8T4V"
             : "G-9670T0JENL",
 };
