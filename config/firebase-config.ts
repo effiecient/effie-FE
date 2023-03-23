189ba5b7eb942bb1cd4e103b9300e59a4e409e87
Christopher Jeffrey <christopherjeffrey@gmail.com>
2023-03-23 18:08:29 +0700

    merge dev handle ci cd

--- config/firebase-config.ts
+++ config/firebase-config.ts
@@ -1,21 +1,21 @@
 // Ini yang development
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDJxdWsoTvciQBfw2OIa0jvuR9l_xEAIls",
-    authDomain: "effie-be-dev.firebaseapp.com",
-    projectId: "effie-be-dev",
-    storageBucket: "effie-be-dev.appspot.com",
-    messagingSenderId: "1047258247146",
-    appId: "1:1047258247146:web:639f0e3011277a57cb8c93",
-    measurementId: "G-ERZQTH8T4V",
-};
+// export const FIREBASE_CONFIG = {
+//     apiKey: "AIzaSyDJxdWsoTvciQBfw2OIa0jvuR9l_xEAIls",
+//     authDomain: "effie-be-dev.firebaseapp.com",
+//     projectId: "effie-be-dev",
+//     storageBucket: "effie-be-dev.appspot.com",
+//     messagingSenderId: "1047258247146",
+//     appId: "1:1047258247146:web:639f0e3011277a57cb8c93",
+//     measurementId: "G-ERZQTH8T4V",
+// };
 
 // Ini yang production
-// export const FIREBASE_CONFIG = {
-//     apiKey: "AIzaSyBIlSbwlKUovrLptRCcByKoesYU8rZXMlw",
-//     authDomain: "effie-be.firebaseapp.com",
-//     projectId: "effie-be",
-//     storageBucket: "effie-be.appspot.com",
-//     messagingSenderId: "1094450506359",
-//     appId: "1:1094450506359:web:6f7b97eefc899603dd3472",
-//     measurementId: "G-9670T0JENL",
-// };
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBIlSbwlKUovrLptRCcByKoesYU8rZXMlw",
+    authDomain: "effie-be.firebaseapp.com",
+    projectId: "effie-be",
+    storageBucket: "effie-be.appspot.com",
+    messagingSenderId: "1094450506359",
+    appId: "1:1094450506359:web:6f7b97eefc899603dd3472",
+    measurementId: "G-9670T0JENL",
+};
