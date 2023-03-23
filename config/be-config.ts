0f0a43f8bd88ebc5c455cecc425206e130091cef
Christopher Jeffrey <christopherjeffrey@gmail.com>
2023-03-23 18:07:22 +0700

    Squashed commit of the following:
    commit 1717bc0f32ff250eb3764449b978965af1c53bdd
    Author: Christopher Jeffrey <christopherjeffrey@gmail.com>
    Date:   Thu Mar 23 17:52:12 2023 +0700
    
        fix firebase config
    
    commit 7d8e4089eb0fc8a9f38e5d35047320fc35f9d902
    Author: Christopher Jeffrey <christopherjeffrey@gmail.com>
    Date:   Thu Mar 23 17:39:11 2023 +0700
    
        debugging by printing env
    
    commit a5090ddb53fc96019b8338618597666e9b641551
    Author: Christopher Jeffrey <christopherjeffrey@gmail.com>
    Date:   Thu Mar 23 17:34:14 2023 +0700
    
        fixing: env again
    
    commit 7de666db62f186cc0ad29bdc9253ed31eb29ee33
    Author: Christopher Jeffrey <christopherjeffrey@gmail.com>
    Date:   Thu Mar 23 17:23:18 2023 +0700
    
        debuging vercel env
    
    commit 8e5b1de33e0c7337f00a64be48659017bad3403c
    Author: Christopher Jeffrey <christopherjeffrey@gmail.com>
    Date:   Thu Mar 23 17:21:25 2023 +0700
    
        dbuging vercel env
    
    commit 80fe77f6bda58640fc4094a732f9a0e6d8d1b66e
    Author: Christopher Jeffrey <christopherjeffrey@gmail.com>
    Date:   Thu Mar 23 17:19:59 2023 +0700
    
        debuging vercel env
    
    commit a26bf0681f2c79dc983db00e23047e0ba3c5d353
    Author: Christopher Jeffrey <christopherjeffrey@gmail.com>
    Date:   Thu Mar 23 17:18:25 2023 +0700
    
        fixing fe config misatke
    
    commit 97da027be9d6c510eb99a5c7e873f8e4c91a637f
    Author: Christopher Jeffrey <christopherjeffrey@gmail.com>
    Date:   Thu Mar 23 17:17:00 2023 +0700
    
        renaming node_env to vercel_env
    
    commit 91d90586857e310c9b37671e465606000b5d6e88
    Author: Christopher Jeffrey <christopherjeffrey@gmail.com>
    Date:   Thu Mar 23 17:10:32 2023 +0700
    
        trigering vercel dev

--- config/be-config.ts
+++ config/be-config.ts
@@ -1,4 +1,4 @@
-// export const BE_BASE_URL = "http://localhost:8080/api";
+// export const BE_BASE_URL = "http://localhost:3001/api";
 export const BE_BASE_URL =
     process.env.VERCEL_ENV === "production" || process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
         ? "https://api.effie.boo/api"
