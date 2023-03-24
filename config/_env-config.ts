3f3aa02f5349b78440bd0e4634f3ee8cf84c955e
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-03-24 14:00:07 +0700

    adding comment

--- config/_env-config.ts
+++ config/_env-config.ts
@@ -1,3 +1,4 @@
+// https://stackoverflow.com/questions/18276951/how-do-i-stop-git-from-tracking-any-changes-to-a-file-from-this-commit-forward
 // "production" || "development"
 
 export const NODE_ENV: string = "production";
