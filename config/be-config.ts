dcdece572def817ca6d43002ec00d91b14773123
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-03-24 12:49:20 +0700

    fix: adding on create link and folder validation

--- config/be-config.ts
+++ config/be-config.ts
@@ -1,11 +1,11 @@
 import { NODE_ENV } from "./env-config";
 
-// export const BE_BASE_URL = "http://localhost:3001/api";
+export const BE_BASE_URL = "http://localhost:8080/api";
 
-export const BE_BASE_URL =
-    NODE_ENV === "production"
-        ? "https://api.effie.boo/api"
-        : "https://dev.api.effie.boo/api";
+// export const BE_BASE_URL =
+//     NODE_ENV === "production"
+//         ? "https://api.effie.boo/api"
+//         : "https://dev.api.effie.boo/api";
 
 export const BE_STATUS_SUCCESS = "SUCCESS";
 export const BE_STATUS_ERROR = "ERROR";
--- config/env-config.ts
+++ config/env-config.ts
@@ -1,3 +1,3 @@
 // "production" || "development"
 
-export const NODE_ENV: string = "production";
+export const NODE_ENV: string = "development";
