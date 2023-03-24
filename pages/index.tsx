4ffb904e3d9d5744e1641442cb40ab181b171580
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-03-24 14:21:35 +0700

    console logging env

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,10 @@ import { useUserStore } from "@/hooks";
 export default function Index() {
     const isLoggedIn = useUserStore((state: any) => state.isLoggedIn);
     const isSubdomain = useUserStore((state: any) => state.isSubdomain);
-
+    console.log("process.env.VERCEL_ENV");
+    console.log(process.env.VERCEL_ENV);
+    console.log("process.env.NODE_ENV");
+    console.log(process.env.NODE_ENV);
     if (isSubdomain) {
         return (
             <>
