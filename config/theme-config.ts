234254a9e70a0920f04db3e94edbd6d98ebf05c2
FelineJTD <13520050@std.stei.itb.ac.id>
2023-04-26 13:38:22 +0700

    wip: theme

---
+++ config/theme-config.ts
@@ -0,0 +1,23 @@
+const Themes = {
+    "effie" : {
+        "thumbnail-colors": [
+            "#6C5BBA",
+            "#F9B0C3",
+            "#FFC212",
+        ]
+    },
+    "classic-light" : {
+        "thumbnail-colors": [
+            "#8895A7",
+            "#B8C4CE",
+            "#F8F9FA",
+        ]
+    },
+    "classic-dark" : {
+        "thumbnail-colors": [
+            "#000000",
+            "#434D5A",
+            "#667181",
+        ]
+    }
+}
